27dd0448612cb1686f8d6e8d563c8948f097a845
Simon Wroblewski <32956185+wrobbeling@users.noreply.github.com>
2024-02-11 20:33:52 +0100

    Add files via upload

---
+++ Suchen und Ersetzen/SuchenUndErsetzen/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace SuchenUndErsetzen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.feedbackSendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Suchen und Ersetzen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(130, 163);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(261, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 216);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(261, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Suchtext (mit * als Platzhalter)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ersetzen durch...";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(728, 395);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Beenden";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(596, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "C:\\VS Repository\\Testverzeichnis";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Verzeichnispfad eingeben:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(744, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Öffnen";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(186, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ausführungszeit [Std:Min:Sek:MSek]: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(124, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Image = global::SuchenUndErsetzen.Properties.Resources.häkchen_grün;
+            this.label6.Location = new System.Drawing.Point(362, 283);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(133, 318);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(281, 100);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Performance:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(919, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem,
+            this.feedbackSendenToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuItem1.Text = "Hilfe";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.infoToolStripMenuItem.Text = "Über mich";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // feedbackSendenToolStripMenuItem
+            // 
+            this.feedbackSendenToolStripMenuItem.Name = "feedbackSendenToolStripMenuItem";
+            this.feedbackSendenToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.feedbackSendenToolStripMenuItem.Text = "Feedback senden";
+            this.feedbackSendenToolStripMenuItem.Click += new System.EventHandler(this.feedbackSendenToolStripMenuItem_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(107, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 26);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "00";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(447, 323);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(254, 94);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Anzahl der Ersetzungen:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 512);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Suchen und Ersetzen";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem feedbackSendenToolStripMenuItem;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Suchen und Ersetzen/SuchenUndErsetzen/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace SuchenUndErsetzen
+{
+    public partial class Form1 : Form
+    {
+        private static int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label6.Text = "";
+            label5.Text = "";
+            label7.Text = "";
+            this.button1.BackColor = Color.LightGray;
+
+            string strDir = textBox2.Text;
+            string ersatzText = textBox1.Text;
+            string suchenText = textBox3.Text;
+
+            if (string.IsNullOrEmpty(strDir) || string.IsNullOrEmpty(suchenText) || string.IsNullOrEmpty(ersatzText))
+            {
+                MessageBox.Show("Bitte füllen Sie alle Felder aus.", "Hinweis");
+                return;
+            }
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            DateisystemDurchlaufen(strDir, suchenText, ersatzText);
+            sw.Stop();
+
+            string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}", sw.Elapsed.Hours, sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.Milliseconds / 10);
+            label5.Text = elapsedTime;
+            label6.Text = "Fertig!";
+            this.button1.BackColor = Color.Green;
+            this.label7.Text = counter.ToString();
+        }
+
+        public static void DateisystemDurchlaufen(string strDir, string suchenText, string ersatzText)
+        {
+            string pattern = "(\\w*)";
+
+            if (suchenText.Contains("*"))
+            {
+                int foundAsterisk = suchenText.IndexOf("*");
+                suchenText = suchenText.Remove(foundAsterisk, 1);
+                suchenText = suchenText.Insert(foundAsterisk, pattern);
+
+                DateisystemDurchlaufen(new DirectoryInfo(strDir), suchenText, ersatzText);
+            }
+            else
+                DateisystemDurchlaufen(new DirectoryInfo(strDir), suchenText, ersatzText);
+        }
+
+        private static void DateisystemDurchlaufen(DirectoryInfo di, string suchenText, string ersatzText)
+        {
+            try
+            {
+                foreach (FileInfo fi in di.GetFiles())
+                {
+                    string text = File.ReadAllText(fi.FullName, Encoding.UTF8);
+                    text = Regex.Replace(text, suchenText, m => { counter++; return ersatzText; });
+                    File.WriteAllText(fi.FullName, text);
+                }
+
+                foreach (DirectoryInfo diSub in di.GetDirectories())
+                {
+                    DateisystemDurchlaufen(diSub, suchenText, ersatzText);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Info");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label6.Text = "";
+            this.button1.BackColor = Color.LightGray;
+
+            using (FolderBrowserDialog ofd = new FolderBrowserDialog())
+            {
+                DialogResult result = ofd.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    textBox2.Text = ofd.SelectedPath;
+                }
+            }
+        }
+
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Simon Wroblewski\nFachinformatiker Anwendungsentwicklung", "Einstellungstest", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void feedbackSendenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Emailadresse: bewerbungsmappe@kabelmail.de", "Feedback", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
